d3f7b76136c9ab15e4026fa5430e93d828ba9818
tacticaltiger05 <seandorje1@gmail.com>
2024-01-09 03:42:45 -0800

    finished project

--- feed/views.py
+++ feed/views.py
@@ -1,4 +1,7 @@
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, DetailView, FormView
+from .models import Post
+from .forms import PostForm
+from django.contrib import messages
 # Create your views here.
 
 class HomePageView(TemplateView):
@@ -6,5 +9,29 @@ class HomePageView(TemplateView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['my thing'] = 'Whatsup blud'
+        context['posts'] = Post.objects.all().order_by('-id')
         return context
+
+
+class PostDetailView(DetailView):
+    template_name = 'detail.html'
+    model = Post
+
+class AddPostView(FormView):
+    template_name = 'new_post.html'
+    form_class = PostForm
+    success_url ='/'
+
+    def disptach(self, request, *args, **kwargs):
+
+        self.request = request
+        return super().dispatch(request, *args, **kwargs)
+
+    def form_valid(self, form):
+
+        new_object = Post.objects.create(
+            text = form.cleaned_data['text'],
+            image = form.cleaned_data['image'],
+        )
+        messages.add_message(self.request, messages.SUCCESS, 'Your post was successful!')
+        return super().form_valid(form)
--- mysite/settings.py
+++ mysite/settings.py
@@ -27,7 +27,8 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, '/media/')
 # Application definition
 
 INSTALLED_APPS = [
@@ -38,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'feed',
+    'sorl.thumbnail',
 ]
 
 MIDDLEWARE = [
